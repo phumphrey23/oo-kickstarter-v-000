422b0af8ff40dbdf9d09c40268c766a1a060328f
Precious Humphrey <phumphrey23@gmail.com>
2018-06-10 06:58:32 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,12 +1,16 @@
 class Project
   @@backers = []
-  attr_accessor :title
-  
+  attr_accessor :title, :name
+
   def initialize(title)
     @title = title
   end
 
-  def add_backer
+  def add_backer(name)
+    @@backers << name
+  end
+
+  def backers
     @@backers
   end
 
